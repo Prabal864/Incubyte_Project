ed73300be2aa9a11aa842e95d09aa22757e339f8
prabal864 <940pps@gmail.com>
2025-12-14 15:07:11 +0530

    refactor: No structural changes required after username-exists test (REFACTOR)

--- src/test/java/com/micronauticals/incubyte_project/service/AuthServiceTest.java
+++ src/test/java/com/micronauticals/incubyte_project/service/AuthServiceTest.java
@@ -14,11 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
