e96e761931e3451f2e641d0809096cd87ebf2261
prabal864 <940pps@gmail.com>
2025-12-14 14:11:45 +0530

    Initialize Spring Boot project with Maven configuration and basic application setup

---
+++ src/main/java/com/micronauticals/incubyte_project/IncubyteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.micronauticals.incubyte_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncubyteProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncubyteProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/micronauticals/incubyte_project/IncubyteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micronauticals.incubyte_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncubyteProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
