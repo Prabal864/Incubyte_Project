e6cb4c8c44fa32025cfc60f275381e2f30ccd15c
prabal864 <940pps@gmail.com>
2025-12-14 14:13:23 +0530

    feat: add Sweet entity with JPA annotations (GREEN)

---
+++ src/main/java/com/micronauticals/incubyte_project/model/Sweet.java
@@ -0,0 +1,29 @@
+package com.micronauticals.incubyte_project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sweet {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(nullable = false)
+    private String category;
+    
+    @Column(nullable = false)
+    private Double price;
+    
+    @Column(nullable = false)
+    private Integer quantity;
+}
